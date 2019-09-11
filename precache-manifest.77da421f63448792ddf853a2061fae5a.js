f526a28a6e8e1875ccca7dcf57c13a327fbb8000
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-11 03:08:44 +0000

    Deploy abel1105/notebook to github.com/abel1105/notebook.git:gh-pages

---
+++ precache-manifest.77da421f63448792ddf853a2061fae5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27546abd56d10fffad8941c73935e9ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e99c912c04ccf289443e",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c238d45b206c8ab007a8",
+    "url": "/static/css/main.faaeafa9.chunk.css"
+  },
+  {
+    "revision": "e99c912c04ccf289443e",
+    "url": "/static/js/2.6fe2a7ff.chunk.js"
+  },
+  {
+    "revision": "c238d45b206c8ab007a8",
+    "url": "/static/js/main.d7601296.chunk.js"
+  },
+  {
+    "revision": "5a0e76fb7904c02b135f",
+    "url": "/static/js/runtime~main.1d70ca35.js"
+  }
+]);
